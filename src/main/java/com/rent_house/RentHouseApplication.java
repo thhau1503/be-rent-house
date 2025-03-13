705f2ceaec88a377e4f73bd6d75d22322501ceb3
thhau1503 <thanhhaudeb204@gmail.com>
2025-03-13 15:19:10 +0700

    create restful api for rent-house project

---
+++ src/main/java/com/rent_house/RentHouseApplication.java
@@ -0,0 +1,13 @@
+package com.rent_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rent_house/RentHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rent_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
